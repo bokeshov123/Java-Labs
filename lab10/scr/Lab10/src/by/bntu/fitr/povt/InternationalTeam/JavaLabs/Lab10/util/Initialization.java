757a0967438ace76795091fdf20dc5e5be2353c4
Talgat Bokeshov <bokeshov123@gmail.com>
2019-04-05 02:06:42 +0300

    Delete Initialization.java

--- lab10/scr/Lab10/src/by/bntu/fitr/povt/InternationalTeam/JavaLabs/Lab10/util/Initialization.java
+++
@@ -1,8 +0,0 @@
-package by.bntu.fitr.povt.InternationalTeam.JavaLabs.Lab10.util;
-
-import by.bntu.fitr.povt.InternationalTeam.JavaLabs.Lab10.model.entity.Student;
-import java.util.UUID;
-
-public class Initialization {
-    
-}
